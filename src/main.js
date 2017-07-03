0932a8a5fd2b430a8b795629f8597e880379a0be
Akhere <ihenyenpaul@gmail.com>
2017-07-03 17:51:17 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-// module.exports = {
-    function isPrime(i) {
+     function isPrime(i) {
     for (var c = 2; c <= Math.sqrt(i); ++c)
         if (i % c === 0 ) return false;
     return true;
     }
-
+//exports.getPrimes = function(n){
+ module.exports = 
     function getPrimes(n) {
-    var list = [];
-    //   if (n < 2) return 'undefined';
-    for (var i = 2; i != n; ++i)
+    let list = [];
+    if ( n === 0 || n === 1 ) {
+            return false;
+        }
+    for (let i = 2; i != n; ++i)
         if (isPrime(i)) list.push(i);
     return list;
     }
-// };
 
-console.log(getPrimes(10));
+
+//console.log(getPrimes(1));
