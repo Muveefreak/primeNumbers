b2aa5014910104dca50211a2b2b340575e4c8b96
Akhere <ihenyenpaul@gmail.com>
2017-07-03 18:41:11 +0100

    update files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-     function isPrime(i) {
-    for (var c = 2; c <= Math.sqrt(i); ++c)
+function isPrime(i) {
+    for (let c = 2; c <= Math.sqrt(i); ++c) {
         if (i % c === 0 ) return false;
-    return true;
     }
-//exports.getPrimes = function(n){
- module.exports = 
-    function getPrimes(n) {
+    return true;
+}
+
+const getPrimes = n => {
     let list = [];
     if ( n === 0 || n === 1 ) {
             return false;
@@ -13,7 +13,9 @@
     for (let i = 2; i != n; ++i)
         if (isPrime(i)) list.push(i);
     return list;
-    }
+}
 
-
-//console.log(getPrimes(1));
+module.exports = {
+    getPrimes: getPrimes
+}
+console.log(getPrimes(10));
--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,49 @@
-const assert = require('chai').assert;
+"use strict";
 const myApp = require('../src/main.js');
+var expect = require('chai').expect;
 
-describe("primeNumbers", function() {
-
-    describe("Handle valid input", function() {
-        it("should return 7 as prime number", function(){
-            assert.equal(myApp.getPrimes(7), 'true')
+describe('primeNumbers', function() {  
+    describe('#primeNumbers()', function() {
+        it('should return prime number of 1 to be false', function() {
+            expect(myApp.getPrimes(1)).to.be.false;
         });
+    });
+    describe('#primeNumbers()', function() {
+        it('should return prime number of 1 to be [2,3,5,7]', function() {
+            expect(myApp.getPrimes(10)).to.eql([ 2, 3, 5, 7 ]);
+        });
+    });
+});
 
-        // it("should return 120 as factorial for 5", function(){
-        //     assert.equal(myApp.computeFactorial(5), 120)
-        // });
-    })
+// "use strict";
 
-    // describe("Handle invalid input", function() {
-    //     it("should return undefined as factorial for -5", function(){
-    //         assert.equal(myApp.computeFactorial(-5), 'undefined')
-    //     });
+// var expect = require('chai').expect;
 
-    //      it("should return undefined as factorial for 'demo'", function(){
-    //         assert.equal(myApp.computeFactorial('demo'), 'undefined')
-    //     });
-    // })
+// // const assert = require('chai').assert;
+// const myApp = require('../src/main.js');
 
-})
+// expect(myApp.getPrimes(1)).to.be.false; 
+
+// describe("primeNumbers", function() {
+
+//     describe("Handle valid input", function() {
+//         it("should return 10 as prime number", function(){
+//             assert.equal(myApp.getPrimes(10), [2,3,5,7])
+//         });
+
+//         // it("should return 120 as factorial for 5", function(){
+//         //     assert.equal(myApp.computeFactorial(5), 120)
+//         // });
+//     })
+
+//     describe("Handle invalid input", function() {
+//         it("should return false as prime number 0", function(){
+//             assert.equal(myApp.getPrimes(0), 'false')
+//         });
+
+//          it("should return false as prime number 1", function(){
+//             assert.equal(myApp.getPrimes(1), 'false')
+//         });
+//     })
+
+// })
